e1221509bdf37908019e484b1896970e3e66bd4b
Shunsuke Saito <shunsuke15vb@gmail.com>
2019-10-23 14:38:11 +0900

    db初期化処理修正

--- db/sql/000_initdb.sql
+++ db/sql/000_initdb.sql
@@ -1,8 +1,14 @@
-CREATE DATABASE IF NOT EXISTS sample_db;
+CREATE DATABASE IF NOT EXISTS test_db;
 
-USE sample_db;
+CREATE USER 'test_user'@'%' IDENTIFIED BY 'test_user'
+CREATE USER 'test_user'@'localhost' IDENTIFIED BY 'test_user'
 
- CREATE TABLE IF NOT EXISTS person (
+GRANT ALL ON test_db.* TO 'test_user'@'%';
+GRANT ALL ON test_db.* TO 'test_user'@'localhost';
+
+USE test_db;
+
+ CREATE TABLE IF NOT EXISTS sample_table (
     id BIGINT NOT NULL AUTO_INCREMENT,
     user_name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
--- db/sql/001_init.sql
+++
@@ -1,8 +0,0 @@
-USE sample_db;
-
-START TRANSACTION;
-
-INSERT INTO person(user_name) values ("Mark");
-INSERT INTO person(user_name) values ("John")
-
-COMMIT;
